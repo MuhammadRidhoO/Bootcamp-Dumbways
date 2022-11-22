7c320b8fa180d26f7f6e1dfe24f0a3a4df120aae
M Ridho <muhammadridho081997@gmail.com>
2022-11-22 21:51:36 +0700

    golang

---
+++ day 6 golong bootstrap/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	route := mux.NewRouter()
+	route.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("Hello Ridho"))
+	}).Methods("GET")
+
+	route.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("About"))
+	}).Methods("GET")
+
+		port := "5000"
+
+	fmt.Print("Server sedang berjalan di port " + port)
+	http.ListenAndServe("localhost:" + port, route)
+}
