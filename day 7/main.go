cdb52177efa0b204c1ed951ff8edcb1d652db22f
M Ridho <muhammadridho081997@gmail.com>
2022-11-24 21:29:30 +0700

    yang baru

--- day 7/main.go
+++ day 7/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,7 +12,12 @@ import (
 func main() {
 	route := mux.NewRouter()
 
+	route.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
+
 	route.HandleFunc("/", home).Methods("GET")
+	route.HandleFunc("/contact", contact).Methods("GET")
+	route.HandleFunc("/inputproject", inputproject).Methods("GET")
+	route.HandleFunc("/inputproject", theproject).Methods("POST")
 
 	fmt.Println("Server berjalan pada port 5000")
 	http.ListenAndServe("localhost:5000", route)
@@ -22,6 +29,42 @@ func home(w http.ResponseWriter, r *http.Request){
 
 	if err != nil {
 		w.Write([]byte("Massage : " + err.Error()))
+		return
 	}
 	tmpt.Execute(w, nil)
+}
+
+func contact(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	tmpl, err := template.ParseFiles("views/contact.html")
+
+	if err != nil {
+		w.Write([]byte("Message : " + err.Error()))
+		return
+	}
+	tmpl.Execute(w, nil)
+}
+
+func inputproject(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	tmpl, err := template.ParseFiles("views/inputproject.html")
+
+	if err != nil {
+		w.Write([]byte("Message : " + err.Error()))
+		return
+	}
+	tmpl.Execute(w, nil)
+}
+
+func theproject(w http.ResponseWriter, r *http.Request){
+	err := r.ParseForm()
+
+	if err != nil {
+		log.Fatal(err)
+
+		fmt.Println("ini nama project : " + r.PostForm.Get("theproject"))
+		fmt.Println("ini comment : " + r.PostForm.Get("comment"))
+
+		http.Redirect(w, r, "/", http.StatusMovedPermanently)
+	}
 }
